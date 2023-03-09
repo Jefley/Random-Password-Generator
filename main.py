4de67ddd979d15a415a7d75c2f2e17e21f10c26a
Jefley <123775933+Jefley@users.noreply.github.com>
2023-03-09 09:31:07 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from random import choice, randint as ran
+from time import sleep
 
 numb = '0123456789'
 spchar = "?.,><';:/\|][}{=+_-)(*&^%$#@!`~~¿"
@@ -6,7 +7,18 @@ alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
 print (' ')
 print ('Random password: ')
-for t in range(1, ran(8,15)):
-    print (choice(alpha+ numb+ spchar), end='')
-print ('\n ')
-
+while True:
+    for t in range(1, ran(10,15)):
+        print (choice(alpha+ numb+ spchar), end='')
+    print ('\n ')
+    sleep(1)
+    quest = str(input('Get new results? [Y/N]: ')).upper()
+    while quest not in 'YN':
+        print(' ')
+        sleep(1)
+        quest = input('invalid answer, get new results? [Y/N] ').upper()
+    if quest == 'N':
+        print('Program was finished')
+        print(' ')
+        sleep(1)
+        break
